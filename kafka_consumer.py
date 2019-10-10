18515d0b1554bce72d8d03203b73a8232adf2502
lirui <lirui-rl.ext@siemens.com>
2019-10-10 09:32:28 +0800

    add limit of max recodes for every polling

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,12 +1,12 @@
 from kafka import KafkaConsumer
 import time
 
-consumer = KafkaConsumer(group_id='123456', bootstrap_servers=['10.43.35.25:4531'])
-consumer.subscribe(topics=('test_rhj',))
+consumer = KafkaConsumer(group_id='group-id', bootstrap_servers=['000.000.000.000:0000'], auto_offset_reset='earliest')
+consumer.subscribe(topics=(['topic1', 'topic2']))
 
 index = 0
 while True:
-    msg = consumer.poll(timeout_ms=5)  # 从kafka获取消息
+    msg = consumer.poll(timeout_ms=5, max_records=10)  # 从kafka获取消息
     print(msg)
     time.sleep(2)
     index += 1
